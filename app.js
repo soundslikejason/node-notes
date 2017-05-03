79dc0b42f1581ba5ea6881a8a3a909891d598cee
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-03 09:52:43 -0700

    3rd party modules, lodash

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
+// console.log(_.isString(true));
+// console.log(_.isString('Jason'));
+var filteredArray = _.uniq(['Jason', 1, 'Jason', 1, 2, 3, 4]);
+console.log(filteredArray);
 
-console.log('Result', notes.add(3, 5));
+// var res = notes.addNote();
+// console.log(res);
+//
+// console.log('Result', notes.add(3, 5));
 
 // var user = os.userInfo();
 //
