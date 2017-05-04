af90dad581a1aaa9a500174d923526f0f4892050
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-03 20:39:25 -0700

    listing notes

--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -31,7 +29,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 
 var getNote = (title) => {
@@ -50,7 +48,6 @@ var removeNote = (title) => {
 };
 
 var logNote = (note) => {
-  debugger;
   console.log('--');
   console.log(`Title: ${note.title}`);
   console.log(`Body: ${note.body}`);
